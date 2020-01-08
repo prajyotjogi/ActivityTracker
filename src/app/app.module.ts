d946f591d8ece012e78788669779e4d09de52930
prajyotjogi <32704016+prajyotjogi@users.noreply.github.com>
2020-01-08 09:18:21 +0530

    Created my Own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponentComponent } from './demo-component/demo-component.component';
+import { PjComponent } from './pj.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponentComponent,
+    PjComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pj.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "pj",
+  template : `<h2>Welcome PJ</h2>`
+})
+export class PjComponent { }
