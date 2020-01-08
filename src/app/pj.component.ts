601d6f59068eb36d46eaf86249d3d904bf33ddc9
prajyotjogi <32704016+prajyotjogi@users.noreply.github.com>
2020-01-08 09:40:42 +0530

    Used Structural Directives

--- src/app/pj.component.ts
+++ src/app/pj.component.ts
@@ -2,6 +2,25 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "pj",
-  template : `<h2>Welcome PJ</h2>`
+  template : `<h2>Welcome PJ</h2>
+              <p *ngIf="name">Prajyot</p>
+              <p *ngIf="sirname">JOgi</p>
+
+              <!--<div [ngSwitch]="colors">
+                    <p *ngSwitchWhen="'red'" >Red color</p>
+                    <p *ngSwitchWhen="'blue'" >blue color</p>
+                    <p *ngSwitchDefault>Invalid color</p>
+                  </div>
+              -->
+              <ul>
+                <li *ngFor="let color of colors">{{color}}</li>
+              </ul>
+            `
 })
-export class PjComponent { }
+export class PjComponent { 
+  name=true;
+  sirname = false;
+  color = "red";
+  colors = ["red","blue", "green"];
+
+}
